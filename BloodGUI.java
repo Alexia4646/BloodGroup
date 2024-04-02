a31e85950be3028124bac2c7ba809e694d83e9b7
Alexia4646 <157057256+Alexia4646@users.noreply.github.com>
2024-04-02 05:46:19 +0300

    BloodGUI.java

--- BloodGUI.java
+++ BloodGUI.java
@@ -7,30 +7,36 @@ public class BloodGUI extends JFrame {
     private JTextField bloodTypeField;
     private JTextField rhFactorField;
     public BloodGUI(){
+        
         //I am setting the JFrame
         setTitle("Blood Data Information");
         setSize(500,300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         getContentPane().setBackground(new Color(200, 80, 155)); // I set the background color to Mullbery
         setLayout(null);
+        
         // I am creating and adding the Blood Type label
         JLabel bloodTypeLabel = new JLabel("Blood Type:");
         bloodTypeLabel.setBounds(30, 30, 90, 35);
         bloodTypeLabel.setForeground(Color.WHITE);
         add(bloodTypeLabel);
+        
        // I am creating and adding the Blood Type field
         bloodTypeField = new JTextField();
         bloodTypeField.setBounds(130, 30, 210, 35);
         add(bloodTypeField);
+        
         // I am creating and adding the Rh Factor label
         JLabel rhFactorLabel = new JLabel("Rh Factor:");
         rhFactorLabel.setBounds(30, 70, 90, 35);
         rhFactorLabel.setForeground(Color.WHITE);
         add(rhFactorLabel);
+        
         // I am creating and adding the Rh Factor field
         rhFactorField = new JTextField();
         rhFactorField.setBounds(130, 70, 210, 35);
         add(rhFactorField);
+        
         // I am creating and adding the Submit button
         JButton submitButton = new JButton("Submit:");
         submitButton.setBounds(160, 110, 110, 40);
